4c35a93cd714f6bd268d757e86a85fbe9f876196
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-08-20 21:45:19 +0300

    Исправлены тесты

--- sandbox/src/test/java/homeWork/DistanceTests.java
+++ sandbox/src/test/java/homeWork/DistanceTests.java
@@ -6,30 +6,16 @@ import org.testng.annotations.Test;
 public class DistanceTests {
 
     @Test
-    public void test1Distance() {
-        Point p1 = new Point(0, 0);
-        Point p2 = new Point(10, 10);
-        assert p1.distance1(p2) < 15;
-    }
-
-    @Test
-    public void negativeTestDistance1() {
-        Point p1 = new Point(0, 0);
-        Point p2 = new Point(10, 10);
-        assert p1.distance1(p2) != 14.142135623730951;
-    }
-
-    @Test
     public void Test2Distance() {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(10, 10);
-        Assert.assertEquals(p1.distance1(p2), 14.142135623730951);
+        Assert.assertTrue(p1.distance1(p2) == 14.142135623730951);
     }
 
     @Test
-    public void negativeTestDistance2() {
+    public void Test3Distance() {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(10, 10);
-        Assert.assertEquals(p1.distance1(p2), 14.143);
+        Assert.assertNotNull(p1.distance1(p2));
     }
 }
