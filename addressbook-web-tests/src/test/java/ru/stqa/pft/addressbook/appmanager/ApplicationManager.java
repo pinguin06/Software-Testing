250797bae90a78d313162a21fc5e1de00bb354af
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-08-30 13:19:52 +0300

    добавлен SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -8,6 +8,7 @@ import java.time.Duration;
 
 public class ApplicationManager {
     WebDriver wd;
+    private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
  //   private JavascriptExecutor js;
@@ -19,18 +20,8 @@ public class ApplicationManager {
    //     js = (JavascriptExecutor) wd;
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-       wd.get("http://localhost/addressbook/addressbook/");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login("admin", "secret");
     }
 
     public void stop() {
