8c8d7be1a6b0d1e237c5293979be86fe9b7791e8
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-09-04 22:09:25 +0300

    оптимизированы переходы между страницами (3.9)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
 
     public NavigationHelper(WebDriver wd) {
@@ -11,11 +11,18 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void goToGroupPage() {
-
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void goToHomePage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home page"));
         wd.get("http://localhost/addressbook/addressbook/index.php");
     }
