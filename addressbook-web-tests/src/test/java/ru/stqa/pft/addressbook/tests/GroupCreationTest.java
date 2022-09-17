689f5e2792a19a53c19bbe6e35773e801bfb3204
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-09-17 23:18:17 +0300

    Добавлено лямбда-выражение для создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,14 +17,8 @@ public class GroupCreationTest extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        int max = 0;
-        for(GroupData g : after) {
-            if(g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
