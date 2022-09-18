c85cb62b44eba796b36d8c5e3d10c27d7adf359b
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-09-18 23:05:56 +0300

    Добавлена сортировка групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -10,12 +10,11 @@ public class GroupData {
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
-
 
     public GroupData(int id, String name, String header, String footer) {
         this.id = id;
@@ -53,14 +52,13 @@ public class GroupData {
         if (this == o) return true;
         if (!(o instanceof GroupData)) return false;
         GroupData groupData = (GroupData) o;
-        return id == groupData.id && Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(name);
     }
-
     public void setId(int id) {
         this.id = id;
     }
