22fece8a956e0c55c951d0670d63fa34255abdeb
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-10-17 23:24:38 +0300

    Лекция 6.5 загрузка данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,18 +16,21 @@ import static org.testng.Assert.assertEquals;
 public class GroupCreationTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]> ();
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
-        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
-        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
     @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
-      //  GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
         app.group().create(group);
         assertEquals(app.group().count(), before.size() + 1);
         Groups after = app.group().all();
