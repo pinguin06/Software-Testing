98f20dcfc05ced5af9f61ff0802ea45bf521f138
Olga Romanshchak <106147517+pinguin06@users.noreply.github.com>
2022-10-18 23:54:11 +0300

    Лекция 6.2 added ContactDataGenerator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,4 +1,51 @@
 package ru.stqa.pft.addressbook.generators;
 
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactDataGenerator {
+
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);
+
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, file);
+    }
+
+    private static void save(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsoluteFile());
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s;%s;%s\n",
+                    contact.getFirstname(),
+                    contact.getLastname(),
+                    contact.getNickname(),
+                    contact.getMobilePhone(),
+                    contact.getAddress(),
+                    contact.getEmail1()));
+        }
+        writer.close();
+    }
+
+    private static List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData()
+                    .withFirstname(String.format("Olga%s", i))
+                    .withLastname(String.format("Suvorova%s", i))
+                    .withNickname(String.format("nick%s", i))
+                    .withMobilePhone(String.format("12345678%s", i))
+                    .withAddress(String.format("Sity, street, house %s", i))
+                    .withEmail1(String.format("email%s@mail.ru", i)));
+        }
+        return contacts;
+    }
 }
